68757ee694339a3ada9f991f4e5534558f0a33e9
Blythe Sheldon <blythe.sheldon@gmail.com>
2014-02-07 16:24:41 -0800

    added comments.

--- lettercount.py
+++ lettercount.py
@@ -1,13 +1,15 @@
 f = open("twain.txt")
 filetext = f.read().lower()
 
+# the counts dictionary holds two lists: characters (filetext[i])
+# and their respective counts — counts[char]
+
 counts = dict()
 
 for i in range(len(filetext) - 1):
     # get ascii values for lowercase letters only
     if ord(filetext[i]) >= 97 and ord(filetext[i]) <= 122:
         char = filetext[i]
-    
         if char in counts:
             counts[char] += 1
         else:
